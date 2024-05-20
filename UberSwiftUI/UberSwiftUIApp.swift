60599b2143f25c9cb473ec0d0f141ae0c358f21d
Luke king <lukeking404@icloud.com>
2024-05-21 00:02:53 +0800

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by 王佩豪 on 2024/5/21.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
