0ac97af32e5a056bdced4928942c0e24d7b7fff6
Luke king <lukeking404@icloud.com>
2024-05-21 17:11:03 +0800

    6.生成从当前位置到目的地的路线(route)

--- UberSwiftUI/Core/Home/View/UberMapViewRepresentable.swift
+++ UberSwiftUI/Core/Home/View/UberMapViewRepresentable.swift
@@ -32,6 +32,7 @@ struct UberMapViewRepresentable: UIViewRepresentable {
         if let coordiante = locationViewModel.selectedLocationCoordinate{
             print("DEBUG:Selected coordiante In map view is \(coordiante)")
             context.coordinator.addAndSelectAnnotation(withCoordinate: coordiante)
+            context.coordinator.configurePolyline(withDestinationCoordinate: coordiante)
         }
     }
     
@@ -46,6 +47,7 @@ extension UberMapViewRepresentable {
     //MARK: - Coordinator 协调员 ,继承以下两个协议
     class MapCoordinator : NSObject , MKMapViewDelegate {
         let parent: UberMapViewRepresentable
+        var userLocationCoordinate : CLLocationCoordinate2D?
         
         
         //MARK: - Lifecycle
@@ -59,6 +61,7 @@ extension UberMapViewRepresentable {
         //MARK: - MKMapViewDelegate
         //MARK: - 设置用户区域(region)
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            self.userLocationCoordinate = userLocation.coordinate
             let region = MKCoordinateRegion(
                 center: CLLocationCoordinate2D(
                     latitude: userLocation.coordinate.latitude,
@@ -70,16 +73,61 @@ extension UberMapViewRepresentable {
             parent.mapView.setRegion(region, animated: true)
         }
         
+        // 生成polyline
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) ->
+        MKOverlayRenderer {
+            
+            let polyline = MKPolylineRenderer(overlay: overlay)
+            polyline.strokeColor = .systemBlue
+            polyline.lineWidth = 6
+            return polyline
+            
+        }
+        
         //MARK: - helper
+        
         func addAndSelectAnnotation(withCoordinate coordinate : CLLocationCoordinate2D) {
-            // 添加新的pin之前删除原来的标注
+            // 添加新的pin之前,删除原来的标注
             parent.mapView.removeAnnotations(parent.mapView.annotations)
+            
             let anno = MKPointAnnotation()
             anno.coordinate = coordinate
             parent.mapView.addAnnotation(anno)
             parent.mapView.selectAnnotation(anno, animated:true)
             
             parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+        }
+        
+        
+        func configurePolyline(withDestinationCoordinate coordinate : CLLocationCoordinate2D) {
+            guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+            
+            getDestinationRoute(from: userLocationCoordinate , to: coordinate) { route in
+
+                self.parent.mapView.addOverlay(route.polyline)
+            }
+        }
+        
+        func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                                 to destination :CLLocationCoordinate2D,
+                                 completion :@escaping(MKRoute) -> Void) {
+            let userPlaceMark = MKPlacemark(coordinate: userLocation)
+            let destPlaceMark = MKPlacemark(coordinate: destination)
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlaceMark)
+            request.destination = MKMapItem(placemark: destPlaceMark)
+            
+            let direction = MKDirections(request: request)
+            
+            direction.calculate { response,error in
+                if let error = error {
+                    print("DEBUG:Failed to get direction with error : \(error.localizedDescription)")
+                    return
+                }
+                // 这里只取了第一条route,以后可能会需要多条路线,都生成,让用户选择
+                guard let route = response?.routes.first else { return }
+                completion(route)
+            }
         }
     }
 }
